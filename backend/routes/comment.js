234c325f0bc93009f36acbb796a8d82211560592
Antoine Vadot <antoine.vadot@gmail.com>
2021-12-08 08:38:27 +0100

    routes + models done

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const ctrlComment = require('../controllers/comment');
 
-router.delete('/:id_comment', ctrlComment.deleteComment)
+router.delete('/:id', ctrlComment.deleteComment)
 
 module.exports = router;
