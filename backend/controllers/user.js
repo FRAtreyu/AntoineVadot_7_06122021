0eb4228598ddce22c63b8ad9419e528b2701a10c
Antoine Vadot <antoine.vadot@gmail.com>
2021-12-08 16:01:39 +0100

    Modify user OK

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,25 +1,25 @@
 const Model = require('../models/Model');
-
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ ;
 
 exports.getAllUsers = (req, res) => {
     Model.User.findAll({
-        attributes : ['firstname', 'lastname', 'pseudo']
+            attributes: ['firstname', 'lastname', 'pseudo']
         }
     )
         .then(allUsers => {
-            if(allUsers){
+            if (allUsers) {
                 return res.status(201).json(allUsers);
             } else {
-                return res.status(404).json({'error':'no users found'})
+                return res.status(404).json({'error': 'no users found'})
             }
         })
-        .catch(error => res.status(500).json({'error':'failed to fetch users'}))
+        .catch(error => res.status(500).json({'error': 'failed to fetch users'}))
 };
 
 exports.getOneUser = (req, res) => {
-    let userId=req.params.id
+    let userId = req.params.id;
     Model.User.findOne({
-        attributes: ['id','firstname', 'lastname', 'pseudo', 'email'],
+        attributes: ['id', 'firstname', 'lastname', 'pseudo', 'email'],
         where: {id: userId}
     })
         .then(user => {
@@ -35,14 +35,45 @@ exports.getOneUser = (req, res) => {
 
 };
 
-exports.modifyUser = (req, res, next) => {
+exports.modifyUser = (req, res) => {
+    let userId = req.params.id;
+    let pseudo = req.body.pseudo;
+    let email = req.body.email;
+    let emailExist = Model.User.findOne({attributes: ['email'], where: {email: email}});
+    Model.User.findOne({
+        attributes: ['id','pseudo', 'email'],
+        where: {id: userId}
+    })
+        .then(user =>{
+            if(user){
+                if(pseudo!=null){
+                    user.set({
+                        pseudo: pseudo
+                    })
+                }
+                if (email!=null){
+                    if(EMAIL_REGEX.test(email)&&!emailExist) {
+                        user.set({
+                            email: email
+                        })
+                    } else{
+                        return res.status(400).json({'error':'email invalid or already in use'})
+                    }
+                }
+                user.save();
+                return res.status(201).json(user);
+            } else {
+                res.status(404).json({'error':'user not found'})
+            }
+
+        })
+        .catch(error => res.status(500).json({'error':'failed to update user'}))
+};
+
+exports.deleteUser = (req, res) => {
 
 };
 
-exports.deleteUser = (req, res, next) => {
-
-};
-
-exports.getAllUserPosts = (req, res, next) => {
+exports.getAllUserPosts = (req, res) => {
 
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require('../middlewares/auth')
 const ctrlUser = require('../controllers/user');
 
-router.put('/:id', auth,ctrlUser.modifyUser);
+router.put('/:id',ctrlUser.modifyUser);
 router.delete('/:id', auth,ctrlUser.deleteUser);
 router.get('/:id',ctrlUser.getOneUser);
 router.get('/:id/post', ctrlUser.getAllUserPosts)
