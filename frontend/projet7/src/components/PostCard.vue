d8e55291a7769036f86f3844274233f4ace91568
Antoine Vadot <antoine.vadot@gmail.com>
2021-12-13 19:21:44 +0100

    newpost

--- frontend/projet7/src/components/PostCard.vue
+++ frontend/projet7/src/components/PostCard.vue
@@ -52,6 +52,9 @@
 
 export default {
   name: "PostCard",
+  data :() =>({
+
+}),
   components: {}
 }
 </script>
--- frontend/projet7/src/components/Signup.vue
+++ frontend/projet7/src/components/Signup.vue
@@ -70,6 +70,7 @@ export default {
   name: 'Signup',
 
   setup() {
+
     return {
       v$: useVuelidate()
     }
