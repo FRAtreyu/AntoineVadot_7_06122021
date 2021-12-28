e922d87885f0e19558fe1f6439e7adf5836cae5e
Antoine Vadot <antoine.vadot@gmail.com>
2021-12-28 17:17:02 +0100

    suppression de compte

--- frontend/projet7/src/views/Profile.vue
+++ frontend/projet7/src/views/Profile.vue
@@ -1,38 +1,41 @@
 <template>
   <div class="profile">
-    <div class="user__info"><span class="userInfos">Nom</span> : {{userInfo.firstname}} {{ userInfo.lastname }} </div>
+    <div class="user__info"><span class="userInfos">Nom</span> : {{ userInfo.firstname }} {{ userInfo.lastname }}</div>
     <div class="user__info"><span class="userInfos">Pseudo</span> : {{ userInfo.pseudo }}</div>
     <div class="user__info"><span class="userInfos">Email</span> : {{ userInfo.email }}</div>
     <div class="user__info"><span class="userInfos">Nombre de posts</span> : {{ postNumber }}</div>
-    <div class="user__info"><span class="userInfos">Inscrit depuis le </span> : {{ inscriptionDate }}</div>
+    <div class="user__info"><span class="userInfos">Inscrit depuis le</span> : {{ inscriptionDate }}</div>
     <v-form id="avatar_form" name="avatar_form">
-    <v-file-input
-        id="avatar"
-        v-if="userInfo.id===userId"
-        class="avatar__input"
-        :rules="rules"
-        accept="image/png, image/jpeg, image/bmp, image/jpg"
-        placeholder="Pick an avatar"
-        prepend-icon="mdi-camera"
-        label="avatar"
-        name="avatar"
-    ></v-file-input>
+      <v-file-input
+          id="avatar"
+          v-if="userInfo.id===userId"
+          class="avatar__input"
+          :rules="rules"
+          accept="image/png, image/jpeg, image/bmp, image/jpg"
+          placeholder="Pick an avatar"
+          prepend-icon="mdi-camera"
+          label="avatar"
+          name="avatar"
+      ></v-file-input>
     </v-form>
     <v-btn @click="setAvatar" v-if="userInfo.id===userId" class="send_avatar">envoyer</v-btn>
-    <v-btn
-        v-if="this.$cookies.get('role')==='admin'&&this.$route.params.pseudo!=='admin_pseudo'"
-        class="mx-2 delete"
-        fab
-        small
-        color="red"
-        @click="deleteUser"
-    >
-      <v-icon>
-        mdi-delete
-      </v-icon>
-    </v-btn>
+    <div class="delete__user" v-if="this.$cookies.get('role')==='admin'&&this.$route.params.pseudo!=='admin_pseudo' || userInfo.id===userId">
+      <v-btn
+          class="mx-2 delete"
+          fab
+          small
+          color="red"
+          @click="deleteUser"
+      >
+        <v-icon>
+          mdi-delete
+        </v-icon>
+      </v-btn>
+      <span>Supprimer le compte</span>
+    </div>
     <ul class="user__posts">
-      <PostCard  v-for="post in postList" :key="post.id" v-bind:post="post"  @update-like="setUserPosts" @delete-post="setUserPosts"
+      <PostCard v-for="post in postList" :key="post.id" v-bind:post="post" @update-like="setUserPosts"
+                @delete-post="setUserPosts"
       ></PostCard>
     </ul>
   </div>
@@ -52,15 +55,15 @@ export default {
     rules: [
       value => !value || value.size < 2000000 || 'Avatar size should be less than 2 MB!',
     ],
-    userId : '',
-    userPseudo:''
+    userId: '',
+    userPseudo: ''
   }),
 
   computed: {
-    postNumber(){
+    postNumber() {
       return this.postList.length
     },
-    inscriptionDate(){
+    inscriptionDate() {
       let date = this.userInfo.createdAt;
       return new Date(date).toLocaleDateString('fr')
     },
@@ -110,7 +113,7 @@ export default {
       })();
     },
 
-    setAvatar(){
+    setAvatar() {
       let avatarForm = document.getElementById('avatar_form');
       let formData = new FormData(avatarForm);
       console.log(formData);
@@ -134,9 +137,15 @@ export default {
     this.userPseudo = this.$route.params.pseudo;
     await this.setUserInfos()
     await this.setUserPosts()
-    this.userId=Number(localStorage.getItem('userId'))
+    this.userId = Number(localStorage.getItem('userId'))
   },
 
+  async mounted() {
+    this.userPseudo = this.$route.params.pseudo;
+    await this.setUserInfos()
+    await this.setUserPosts()
+    this.userId = Number(localStorage.getItem('userId'))
+  }
 
 
 }
@@ -144,30 +153,36 @@ export default {
 </script>
 
 <style scoped>
-.avatar__input{
+.avatar__input {
   width: 200px;
 }
-.profile{
+
+.profile {
   padding: 20px;
   display: flex;
   flex-direction: column;
 }
 
-.user__posts{
+.user__posts {
   display: flex;
   width: 100%;
   flex-direction: column;
   align-items: center;
 }
 
-.send_avatar{
+.send_avatar {
   width: 90px;
 }
 
-.userInfos{
-  color: black!important;
+.userInfos {
+  color: black !important;
   text-decoration: underline;
   font-size: large;
+}
+
+.delete__user {
+  margin-top: 50px;
+  font-weight: bold;
 }
 
 </style>
